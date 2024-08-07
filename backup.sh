4f1b41992c8dbaffa14860d499d7f6fe7aeb6b3a
rudyon <rudyon>
2024-08-07 22:28:45 +0300

    added description and #!

--- backup.sh
+++ backup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# This script copies files from my home directory into the repository directory
 cp -v ~/.bashrc ~/devl/dotfiles
 cp -rv ~/.config/hypr ~/devl/dotfiles
 cp -rv ~/.config/kitty ~/devl/dotfiles
