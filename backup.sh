4fdab765242fc54aeb96c8039c21471b3d9aa800
rudyon <rudyon>
2024-08-09 23:32:19 +0300

    sync

--- backup.sh
+++ backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script copies files from my home directory into the repository directory
 cp -v ~/.bashrc ~/devl/dotfiles
-cp -rv ~/.config/hypr ~/devl/dotfiles
-cp -rv ~/.config/kitty ~/devl/dotfiles
+cp -rv ~/.config/hypr ~/devl/dotfiles/.config
+cp -rv ~/.config/kitty ~/devl/dotfiles/.config
+cp -rv ~/.config/fish ~/devl/dotfiles/.config
