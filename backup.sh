a22725046c731874f2a4567ecc8657ee16611604
rudyon <rudyon>
2024-08-10 18:17:52 +0300

    Automated backup 2024-08-10 18:17:52

--- backup.sh
+++ backup.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 # This script copies files from my home directory into the repository directory
+
+# Copy files
 cp -v ~/.bashrc ~/devl/dotfiles
 cp -rv ~/.config/hypr ~/devl/dotfiles/.config
 cp -rv ~/.config/kitty ~/devl/dotfiles/.config
 cp -rv ~/.config/fish ~/devl/dotfiles/.config
 cp -rv ~/.config/nvim ~/devl/dotfiles/.config
+
+# Navigate to the dotfiles directory
+cd ~/devl/dotfiles
+
+# Check if there are changes
+if [ -n "$(git status --porcelain)" ]; then
+    # Add changes to git
+    git add -A
+
+    # Commit changes
+    git commit -m "Automated backup $(date +'%Y-%m-%d %H:%M:%S')"
+
+    # Push changes
+    git push origin main
+fi
+
