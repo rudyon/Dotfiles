4b639a2c0b86b2f9ee7ffa9a80eb0e4fd5b79933
rudyon <keremberket@gmail.com>
2024-09-01 02:00:01 +0300

    Automated backup 2024-09-01 02:00:00

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -13,5 +13,4 @@ set -gx GOPATH ~/.go
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias sysinfo='python ~/devl/scripts/sys_overview.py'
-alias tomatoshell="tomatoshell -p p -f"
 alias hyfetch="hyfetch --ascii-file ~/.hyfetch_cachyos_small"
