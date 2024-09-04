b6e6e9fcf6abd1f05a401e88eea0941b0de56863
rudyon <keremberket@gmail.com>
2024-09-05 02:00:01 +0300

    Automated backup 2024-09-05 02:00:01

--- .config/nvim/lua/rudyon/init.lua
+++ .config/nvim/lua/rudyon/init.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " " -- Set leader key before Lazy
+vim.g.maplocalleader = " "
 
 require("rudyon.lazy_init")
 require("rudyon.remap")
--- .config/nvim/lua/rudyon/options.lua
+++ .config/nvim/lua/rudyon/options.lua
@@ -1,6 +1,7 @@
 vim.opt.number = true
 
-vim.opt.tabstop = 4
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 vim.opt.smartindent = true
